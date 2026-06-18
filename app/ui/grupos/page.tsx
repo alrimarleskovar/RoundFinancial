7d89ba3398a1651dec66dbf3138eacb011fcc4c5
caiomodesti <129246601+caiomodesti@users.noreply.github.com>
2026-06-18 19:48:44 -0300

    Create Page component for grupos route

---
+++ app/ui/grupos/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{padding:40,color:"white",background:"#050816",minHeight:"100vh"}}>
+      <h1>Teste Grupos</h1>
+      <p>Se você está vendo isso, a rota funcionou.</p>
+    </div>
+  );
+}
